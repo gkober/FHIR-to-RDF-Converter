c0e80d6bababb4b82e2c78285026032551156caf
gkober <48680286+gkober@users.noreply.github.com>
2019-12-27 11:59:54 +0100

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -36,48 +36,25 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.model.api.IResource;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 
-public class Parser {
-	
+public class Parser {	
 	static CDRConnector connector = null;
 	static IBaseBundle b = null;
 	static Model model = null; //apache-jena-model
 	public Model convertFhirToRdf(List<Resource> res,int resourcesToCopy) throws IOException {
-		long start = System.currentTimeMillis(); 
-		long startCreateParser = System.currentTimeMillis(); 
 		RdfParser parser = new RdfParser();
-		long endCreateParser = System.currentTimeMillis(); 
-		long startCreateModel = System.currentTimeMillis(); 
 		model = ModelFactory.createDefaultModel();
 		model.setNsPrefix("fhir", "http://hl7.org/fhir/");
-		long endCreateModel = System.currentTimeMillis(); 
 		//helping to create a new bundle out of the resources, to pass it to the RdfParser
-		//Bundle b = new Bundle();
 		InputStream in = null;
 		ByteArrayOutputStream bOutput = new ByteArrayOutputStream();
-		long start1 = System.currentTimeMillis(); 
 		for (int i=0; i<=resourcesToCopy;i=i+1) {
 			for (Resource element : res) {
 				bOutput.write(parser.composeString(element).toString().getBytes());
-				//System.out.println(parser.composeString(element).toString());
 			}
 		}
-		long start2 = System.currentTimeMillis(); 
 		byte by [] = bOutput.toByteArray();
-		//in = new ByteArrayInputStream(by);
 		in = new ByteArrayInputStream(bOutput.toByteArray());
-		//System.out.println(connector.getCtx().newXmlParser().setPrettyPrint(true).encodeResourceToString(b));
-		long start3 = System.currentTimeMillis(); 
 		model.read(new ByteArrayInputStream(bOutput.toByteArray()),null,"TTL");
-		//model.read(new ByteArrayInputStream(bOutput.toByteArray()),null,"JSONLD");
-		long end = System.currentTimeMillis(); 
-		//System.out.println("convert - Time taken complete: " +  (end - start) + "ms" + " for " + resourcesToCopy +
-		//		 " entries"); 
-		//System.out.println("convert - Time taken reading resources: " +  (start2 - start1) + "ms"); 
-		//System.out.println("convert - Time taken creating the model: " +  (endCreateModel - startCreateModel) + "ms"); 
-		//System.out.println("convert - Time taken creating the parser: " +  (endCreateParser - startCreateParser) + "ms"); 
-		System.out.println("convert - Time taken reading inputStream to model: " +  (end - start3) + "ms" + " for " + resourcesToCopy +
-				 " entries"); 
-		//model.read(in,null,"TTL");
 		return model;
 	}	
 	public List<Resource> bundleToResource(Bundle bundle) throws FHIRException, FHIRFormatError, UnsupportedEncodingException, IOException{
@@ -91,7 +68,7 @@ public class Parser {
 	public void printModel() {
 		this.model.write(System.out);
 	}
-	
+	/** Starting the testprogramm **/
 	public static void main(String[] args) throws FHIRException, FHIRFormatError, UnsupportedEncodingException, IOException {
 		Parser p = new Parser();
 		for (int i = 1; i<=10;i=i+10) {
@@ -99,21 +76,13 @@ public class Parser {
 			connector = new CDRConnector("R4", "http://test.fhir.org/r4");
 			connector.connect();
 			b = connector.searchForAllResources("Observation",1);
-			
 			Bundle bundle = (Bundle)b;
 			bundle.getTotal();
 			List<Resource> r = p.bundleToResource(bundle);
-			long endconnection = System.currentTimeMillis(); 
-			long startconversion = System.currentTimeMillis(); 
 			for(int j=0;j<=1000;j=j+1000)
-			model = p.convertFhirToRdf(r,j);
+				model = p.convertFhirToRdf(r,j);
 			model.write(System.out);
-			long end = System.currentTimeMillis(); 
-	    System.out.println("Time taken complete: " +  (end - start) + "ms"); 
-	    System.out.println("Time connection taken: " +  (endconnection - start) + "ms");
-	    System.out.println("Time conversion taken: " +  (end - startconversion) + "ms"); 
-	    System.out.println("=================");
-	    Parser.model=null;
+	    		Parser.model=null;
 		}
 		System.exit(0);
 	}
